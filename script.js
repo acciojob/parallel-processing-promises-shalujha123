b9de842ca690f1c72dc55beb989e3f98b5db061c
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-03-19 08:45:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,59 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImage(url){
+	return new Promise((resolve,reject)=>{
+		const img = new Image()
+		img.src = url
+		img.onload = () => resolve(img)
+		img.onerror = () => reject(new Error("Failed to load image"))
+	})
+}
+
+function downloadImages(){
+	const promises = images.map(image => downloadImage(image.url))
+	let load = document.createElement("div")
+	load.id = "loading"
+	load.textContent = "Loading..."
+	output.append(load)
+	Promise.all(promises)
+	.then((images) =>{
+		load.remove()
+		images.forEach(image =>{
+			output.append(image)
+		})
+	})
+	.catch(()=>{
+		load.remove()
+		let err = document.createElement("div")
+		err.id = "error"
+		err.textContent = "Failed to load images"
+		output.append(err)
+	})
+}
+
+btn.addEventListener("click",downloadImages)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
